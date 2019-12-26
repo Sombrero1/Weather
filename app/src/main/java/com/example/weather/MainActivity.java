0becbace910d9b7187ffda56eb05b4e320174ed7
Sombrero1 <56596227+Sombrero1@users.noreply.github.com>
2019-12-26 12:21:23 +0300

    ЯндексПереводчик

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,101 +1,113 @@
 package com.example.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
     private TextView tv;
-
-
+    private EditText edit;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
 
-      //   tv = findViewById(R.id.textView);
+        edit=findViewById(R.id.editText);
 
-        btn= findViewById(R.id.button);
+
+
+
+        tv = findViewById(R.id.textView);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //Вызов AsyncTask
+            public void onClick(View view) {
+                // вызов AsyncTask
                 // создание отдельного потока
 
-                MyDownload mytask= new MyDownload();
+                text= edit.getText().toString(); //считываем текст по нажатию
+                MyDownload mytask = new MyDownload();
                 mytask.execute();
-
-
-
-
             }
         });
-
     }
 
-    private class MyDownload extends AsyncTask<Void, Void, Bitmap>{
-        ImageView bmImage;
+    private class MyDownload extends AsyncTask<Void, Void, String> {
 
-            HttpURLConnection httpurl;
+        HttpURLConnection httpurl;
 
-    @Override
-        protected Bitmap doInBackground(Void... voids) {
+        @Override
+        protected String doInBackground(Void... voids) {
+
             try {
-               // URL url=new URL("http://api.weatherstack.com/current?access_key=6d3ecccfce14b8bf1efaba43c5c8f18e&query=Krasnodar");
-              //  httpurl= (HttpURLConnection) url.openConnection();
-              //  httpurl.setRequestMethod("GET");
-               // httpurl.connect();
-                Bitmap mIcon11 = null;
-                InputStream in = new java.net.URL("http://www.playcast.ru/uploads/2015/11/07/15780970.png").openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
 
-                //InputStream inputStream = httpurl.getInputStream();
-                //Scanner scan=new Scanner(inputStream);
-                //StringBuffer buffer= new StringBuffer();
-               // while (scan.hasNextLine()){
 
-                //    buffer.append(scan.nextLine());
 
-                ///}
-                return mIcon11;
+                String v="https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191226T080016Z.119e512f37c2e2f1.054a00bcaf9ff4d6d65db962bfdf982829706e54&text=" + text +"&lang=en-ru&format=plain" ;
+                URL url = new URL(v);
+                httpurl = (HttpURLConnection) url.openConnection();
+                httpurl.setRequestMethod("GET");
+                httpurl.connect();
+
+                InputStream inputStream = httpurl.getInputStream();
+
+                Scanner scan = new Scanner(inputStream);
+                StringBuffer buffer = new StringBuffer();
+                while(scan.hasNextLine()) {
+                    buffer.append(scan.nextLine());
+                }
+                return buffer.toString();
 
             } catch (java.io.IOException e) {
                 e.printStackTrace();
-                Log.e("RRRR",e.toString());
+                //Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                Log.e("RRRRR",e.toString());
             }
-
 
 
             return null;
         }
 
         @Override
-        protected void onPostExecute(Bitmap s) { // метод, который выполняется после doInBackround
+        protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            bmImage=findViewById(R.id.imageView);
-            bmImage.setImageBitmap(s);
+            // метод, который выполняется после doInBackground
 
 
-        }
+
+            Log.e("RRRRRRRRR",s);
+
+
+
+          Gson g = new Gson();
+          Yandex yandex=g.fromJson(s,Yandex.class);
+          List<String> a=yandex.getText();
+
+            tv.setText(a.get(0));
+
+
+
+
+
+
     }
-
-
-}
+}}
